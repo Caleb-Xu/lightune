5af90db62f1422b426a17277b7c2f295217c5d41
Caleb-Xu <noname1427968964@outlook.com>
2023-10-29 00:48:05 +0800

    feat: 格式化配置更新

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+/** @type { import('prettier').Config } */
+module.exports = {
+    singleQuote: true,
+    semi: false,
+    printWidth: 100,
+    trailingComma: 'none',
+    endOfLine: 'auto',
+    tabWidth: 4
+}
